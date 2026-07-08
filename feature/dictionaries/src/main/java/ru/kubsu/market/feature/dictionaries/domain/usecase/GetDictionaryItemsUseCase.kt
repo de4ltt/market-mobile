9fad100d44e2f93ee3c0f8568885fa06c8945398
Aleksej Timko <aleksejtmk@gmail.com>
2026-07-08 17:52:34 +0300

    fix(dictionaries): add missing @Inject constructor to GetDictionaryItemsUseCase

--- feature/dictionaries/src/main/java/ru/kubsu/market/feature/dictionaries/domain/usecase/GetDictionaryItemsUseCase.kt
+++ feature/dictionaries/src/main/java/ru/kubsu/market/feature/dictionaries/domain/usecase/GetDictionaryItemsUseCase.kt
@@ -1,15 +1,13 @@
 package ru.kubsu.market.feature.dictionaries.domain.usecase
 
-import ru.kubsu.market.core.model.IDictionaryItem
-import ru.kubsu.market.core.model.IItemRepresentable
-import ru.kubsu.market.feature.dictionaries.domain.DictionariesRepository
-
 import javax.inject.Inject
+import ru.kubsu.market.core.model.IDictionaryItem
+import ru.kubsu.market.feature.dictionaries.domain.DictionariesRepository
 
 class GetDictionaryItemsUseCase @Inject constructor(
     private val repository: DictionariesRepository
 ) {
-    suspend operator fun invoke(item: IDictionaryItem): List<IItemRepresentable> {
+    suspend operator fun invoke(item: IDictionaryItem): List<IDictionaryItem> {
         return repository.getItems(item)
     }
 }
